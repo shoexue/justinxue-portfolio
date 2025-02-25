f7ea33032589ab4a0e16d95eeb4f8e31b8fca19b
alvina-yang <alvinay73@gmail.com>
2025-02-24 21:08:25 -0500

    logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,11 @@ import StyledComponentsRegistry from '../lib/registry'
 export const metadata = {
   title: 'Alvina Yang',
   description: 'Personal Portfolio',
+  icons: {
+    icon: '/icon.png',
+    shortcut: '/icon.png',
+    apple: '/icon.png',
+  },
 }
 
 export default function RootLayout({ children }) {
