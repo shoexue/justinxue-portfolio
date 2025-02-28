baa7162b80aaa44d22e033b4441c3b76847c81c6
alvina-yang <alvinay73@gmail.com>
2025-02-28 12:53:07 -0500

    work

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+  // Ensure assets are copied to the output directory
+  distDir: 'out',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://alvinayang.com' : '',
 }
 
 module.exports = nextConfig 
