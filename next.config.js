7180ef1122ed5fea2be0319db973130a21d35d1a
Justin Xue <43502772+shoexue@users.noreply.github.com>
2025-07-03 17:44:16 -0400

    sdfs

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
+  trailingSlash: true, 
   // assetPrefix: process.env.NODE_ENV === 'production' ? 'https://alvinayang.com' : '',
 }
 
