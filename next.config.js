0dc52f18e54b20b8e3d334bd5a5ea07fa55fc458
Justin Xue <43502772+shoexue@users.noreply.github.com>
2025-07-03 23:28:34 -0400

    sdfsd

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
   },
   trailingSlash: true,
   // Ensure assets are copied to the output directory
-  // distDir: 'out',
-  // assetPrefix: process.env.NODE_ENV === 'production' ? 'https://alvinayang.com' : '',
+  distDir: 'out',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://justinxue-portfolio.vercel.app/' : '',
 }
 
 module.exports = nextConfig 
