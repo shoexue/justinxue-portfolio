e4813fdd8b91224965064aca7ea3ef6bf8d30aa8
Justin Xue <43502772+shoexue@users.noreply.github.com>
2025-07-03 23:20:32 -0400

    sdf

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true, 
-  // assetPrefix: process.env.NODE_ENV === 'production' ? 'https://alvinayang.com' : '',
+  trailingSlash: true,
+  // Ensure assets are copied to the output directory
+  distDir: 'out',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://alvinayang.com' : '',
 }
 
 module.exports = nextConfig 
